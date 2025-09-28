de72b3c8910d66b37c0c4468dcd5ab80c00f36a8
Madalina C <costovim@tcd.ie>
2025-09-28 19:03:38 +0100

    Completed exercise by creating Handbag class

--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,34 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+
+import Backpack from "./Backpack.js";
+
+class Handbag extends Backpack {
+  constructor(
+    name,
+    volume,
+    color,
+    pocketNum,
+    strapLengthL,
+    strapLengthR,
+    lidOpen,
+    brand
+  ) {
+    super(name, volume, color, pocketNum, strapLengthL, strapLengthR, lidOpen);
+    this.brand = brand;
+  }
+  toggleLid(lidStatus) {
+    super.toggleLid(lidStatus);
+    if (lidStatus) {
+      console.log("Open! Make sure to close when finished!");
+    } else {
+      console.log("Closed!");
+    }
+  }
+}
+
+const handbag = new Handbag("Handbag", 25, "black", 1, 0, 0, false, "Gucci");
+console.log(handbag);
+
+export default Handbag;
