b5817a68fc6792cd119d8891d3794135fd23de26
Madalina C <costovim@tcd.ie>
2025-09-28 17:10:07 +0100

    Completed exercise by adding function to change colour of bag

--- Practice/03_09/script.js
+++ Practice/03_09/script.js
@@ -23,4 +23,10 @@ const backpack = {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   },
+
+  newColor: function (color) {
+    this.color = color;
+  },
 };
+
+console.log(backpack);
